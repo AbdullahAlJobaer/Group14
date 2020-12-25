01b52e50bb1292c80cacfb0ad16ad93fda6f360b
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2020-12-25 23:23:09 +0600

    Enhancement #8: Update index.js

--- Code/Practice/Demo eCommerce Practice/index.js
+++ Code/Practice/Demo eCommerce Practice/index.js
@@ -66,3 +66,21 @@ Array.from(scrollLink).map(link => {
     document.body.classList.remove("active");
   });
 });
+
+// Fix NavBar
+
+window.addEventListener("scroll", e => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navBar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix__nav");
+  } else {
+    navBar.classList.remove("fix__nav");
+  }
+
+  if (scrollHeight > 300) {
+    gotoTop.classList.add("show-top");
+  } else {
+    gotoTop.classList.remove("show-top");
+  }
+});
