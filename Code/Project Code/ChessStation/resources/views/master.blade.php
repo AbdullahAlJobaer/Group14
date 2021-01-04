2bdac75e1b2dac74ea8d287aaa60248478905976
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2021-01-04 16:27:01 +0600

    Enhancement #6: Create master.blade.php

---
+++ Code/Project Code/ChessStation/resources/views/master.blade.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chess Station</title>
+    <!-- Latest compiled and minified CSS -->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    {{View::make('header')}}
+     @yield('content')
+     {{View::make('Footer')}}
+</body>
+</html>
