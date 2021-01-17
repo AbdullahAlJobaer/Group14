2a6b3b8dd67300fe2a1a9acdc286def4673b2041
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2021-01-17 20:46:14 +0600

    Enhancement #13: Deleted and recreated migration file for products' database

--- Code/Project Code/ChessStation/database/migrations/2021_01_17_132149_create_products_table.php
+++ Code/Project Code/ChessStation/database/migrations/2021_01_17_132149_create_products_table.php
@@ -17,8 +17,8 @@ class CreateProductsTable extends Migration
             $table->id();
             $table->string("name");
             $table->string("price");
-            $table->string("category");
             $table->string("description");
+            $table->string("category");            
             $table->string("gallery");            
             $table->timestamps();
         });
