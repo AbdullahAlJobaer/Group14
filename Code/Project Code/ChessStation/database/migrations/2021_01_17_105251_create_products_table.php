08e2ee7e23f43ee8dcefab6669af5d1488ebbd42
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2021-01-17 17:00:51 +0600

    Enhancement #13: Updated migration file for products' database

--- Code/Project Code/ChessStation/database/migrations/2021_01_17_105251_create_products_table.php
+++ Code/Project Code/ChessStation/database/migrations/2021_01_17_105251_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("price");
+            $table->string("category");
+            $table->string("description");
+            $table->string("gallery");            
             $table->timestamps();
         });
     }
