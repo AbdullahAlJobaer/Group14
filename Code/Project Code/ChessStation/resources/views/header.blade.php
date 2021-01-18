69efb02f6cdf34dbeec0b100d5e4c410d7c7bcce
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2021-01-18 12:15:05 +0600

    Enhancement #18:  Created Login and Logut with Session

--- Code/Project Code/ChessStation/resources/views/header.blade.php
+++ Code/Project Code/ChessStation/resources/views/header.blade.php
@@ -1,6 +1,12 @@
 <?php
   use App\Http\Controllers\ProductController;
-  $total = ProductController::cartItem();
+  $total=0;
+  if(Session::has('user'))
+  {
+    $total = ProductController::cartItem();
+  }
+  
+
 ?>
 
 <nav class="navbar navbar-default">
@@ -13,7 +19,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Chess Station</a>
+      <a class="navbar-brand" href="/">CHESS STATION</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -30,8 +36,19 @@
         <button type="submit" class="btn btn-default">Search</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
+      @if(Session::has('user'))
         <li><a href="#">Cart({{$total}})</a></li>
-        
+        <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="/logout">Logout</a></li>
+          
+        </ul>
+      </li>
+        @else
+        <li><a href="/login">Login</a></li>
+        @endif
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
